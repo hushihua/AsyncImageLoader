0f5d2f3875a8ffb30db716fbc4cc8b919f24d129
pritywawa <pritywawa@163.com>
2016-03-10 11:16:42 +0800

    create

---
+++ src/android/library/asyncimageloader/ImageRequestSchedule.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package android.library.asyncimageloader;
+
+import android.widget.ImageView;
+
+/**
+ * @author hushihua
+ *
+ */
+class ImageRequestSchedule {
+
+	public ImageView imageView;
+	public String urlRef;
+	public float round;
+	
+}
